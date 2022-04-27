3b4a55948c80bc8bc4e72598d838d3f0d4427777
luis10g <joicefernandasouzaoliveira@gmail.com>
2022-04-27 17:56:26 +0000

    gradlegs

--- src/main/java/gradlegs/App.java
+++ src/main/java/gradlegs/App.java
@@ -2,6 +2,11 @@ package gradlegs;
 
 public class App {
     public static void main(String[] args) {
+        Genero g = new Genero();
+        g.setId(-10);
+        g.setNome("Aventura");
+        
         System.out.println("Olá Gradle");
+        System.out.println(g.getNome());
     }
 }
