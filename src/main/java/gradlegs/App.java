4700843167006892bee0b6c0d28c0004ae66a339
luis10g <joicefernandasouzaoliveira@gmail.com>
2022-05-02 17:57:50 +0000

    gradlegs

--- src/main/java/gradlegs/App.java
+++ src/main/java/gradlegs/App.java
@@ -3,8 +3,12 @@ package gradlegs;
 public class App {
     public static void main(String[] args) {
         Genero g = new Genero();
-        g.setId(-10);
+        g.setId(10);
         g.setNome("Aventura");
+
+        PessoaFisica p = new PessoaFisica();
+        p.setNome("José da Silva");
+        p.setCpf("65498712354");
         
         System.out.println("Olá Gradle");
         System.out.println(g.getNome());
